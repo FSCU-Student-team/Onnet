1a61e8d01898d988e3c6880909379560653e78e5
teracura <abdallahelbhary39@gmail.com>
2025-12-03 23:20:36 +0200

    add Page

---
+++ src/Pages/Page.java
@@ -0,0 +1,20 @@
+package Pages;
+
+import java.awt.event.ActionEvent;
+
+public interface Page {
+    void init();
+    void setupFrame();
+    void setupAnimator();
+    void addComponents();
+
+    void addListeners();
+
+    void handleEvents(ActionEvent e);
+
+    void dispose();
+
+    boolean isVisible();
+
+    void setVisible(boolean b);
+}
