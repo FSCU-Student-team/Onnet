04cdf35c98c4ec509e00cc0dc12cb8cf2b0b618b
teracura <abdallahelbhary39@gmail.com>
2025-12-04 15:29:11 +0200

    add collions from circle to other objects

---
+++ src/Physics/Collision/AABB.java
@@ -0,0 +1,5 @@
+package Physics.Collision;
+
+// Axis-aligned bounding box, contains bounds of (x,y) positions
+public record AABB(double minX, double minY, double maxX, double maxY) {}
+
--- src/Shapes/Shape.java
+++ src/Shapes/Shape.java
@@ -1,5 +1,6 @@
 package Shapes;
 
+import Physics.Collision.Collider;
 import com.jogamp.opengl.GL2;
 
 public interface Shape {
