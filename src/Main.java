5fb815743ce1439ccbc0797dcfa0d1bd54e64d63
teracura <abdallahelbhary39@gmail.com>
2025-12-05 20:13:23 +0200

    remove worthless singleton declaration

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
         //IMPORTANT DEV NOTE:
         //SET VM OPTIONS IN RUN CONFIGURATION TO THIS TO RUN: -Djava.library.path=Libs\Natives\Windows --enable-preview --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED
         //IF USING LINUX CHANGE Libs\Natives\Windows WITH Libs\Natives\Linux
-        GLProfile.initSingleton();
         PageManager.init();
 
         Page test = new DevTestScene();
