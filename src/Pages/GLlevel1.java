2ba072f167248dae8b761ae339a7524521da7588
Abdo <Body16005@gmail.com>
2025-12-08 18:55:55 +0200

    level1

--- src/Pages/GLlevel1.java
+++ src/Pages/GLlevel1.java
@@ -1,26 +1,62 @@
 package Pages;
 
 import Game.GameLoop;
+import Game.Input;
 import Game.InputManager;
 import Game.LoopState;
 import Physics.ActionManager;
 import Renderers.EntityUtils;
-import Shapes.Circle;
+import Shapes.*;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 
-public class GLlevel1 implements GLEventListener , GameLoop {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GLlevel1 implements GLEventListener, GameLoop {
     private InputManager inputManager;
     private ActionManager actionManager;
-    private EntityUtils entityUtils=new EntityUtils();
-    private Circle playerCircle,Goal;
-    private double MaxPower=100;
-    private double minPower=0;
+    private LoopState loopState;
+    private EntityUtils entityUtils = new EntityUtils();
+    private Circle playerCircle;
+    private Rectangle GoalRectangle;
+    private double MaxPower = 100;
+    private double currentPower = 0;
+    private Vector2 Force = new Vector2(0, 0);
+    private Vector2 gravity = new Vector2(0, 0);
+    private double minPower = 0;
+    private double angle = 0;
+    private List<Shape> shapes = new ArrayList<>();
+    private boolean isLunched = false;
 
+    public GLlevel1() {
+        inputManager = new InputManager();
+    }
 
     @Override
     public void init(GLAutoDrawable glAutoDrawable) {
+        loopState = new LoopState();
+        actionManager = new ActionManager(inputManager);
+        GL2 gl = glAutoDrawable.getGL().getGL2();
+        gl.glClearColor(0, 0, 0, 1);
+
+        gl.glMatrixMode(GL2.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glOrtho(0, 800, 0, 600, -1, 1);
+
+        actionManager.bind(Input.A, () -> setAngle(angle + 1));
+        actionManager.bind(Input.S, () -> setCurrentPower(currentPower - 1));
+        actionManager.bind(Input.D, () -> setAngle(angle - 1));
+        actionManager.bind(Input.W, () -> setCurrentPower(currentPower + 1));
+        actionManager.bind(Input.Z, () -> isLunched = true);
+        actionManager.bind(Input.Escape, this::togglePause);
+
+        playerCircle =new Circle.Builder().color(Color.BLUE).radius(20).angle(0).center(new Point(50,40)).filled(true).build();
+        GoalRectangle =new Rectangle.Builder().color(Color.YELLOW).width(20).height(20).fill(false).origin(new Point(500,500)).build();
+
+        entityUtils.addShape(playerCircle);entityUtils.addShape(GoalRectangle);
+        shapes=entityUtils.getShapes();
 
     }
 
@@ -51,7 +87,7 @@ public class GLlevel1 implements GLEventListener , GameLoop {
 
     @Override
     public void inputUpdate() {
-      inputManager=new InputManager();
+        actionManager.update();
     }
 
     @Override
@@ -77,5 +113,24 @@ public class GLlevel1 implements GLEventListener , GameLoop {
     @Override
     public void handleLoop(LoopState state, GL2 gl) {
         GameLoop.super.handleLoop(state, gl);
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle % 360;
+    }
+
+    public double getCurrentPower() {
+        return currentPower;
+    }
+
+    public void setCurrentPower(double currentPower) {
+        if (currentPower < minPower)
+            this.currentPower = minPower;
+        else
+            this.currentPower = currentPower;
     }
 }
