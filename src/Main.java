72d03bc4becdf85db604b17bb98452522752a75c
teracura <abdallahelbhary39@gmail.com>
2025-12-09 18:18:01 +0200

    add important comment

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import Pages.*;
 
 import java.util.ArrayList;
 
+//IMPORTANT DEV NOTE:
+//SET VM OPTIONS IN RUN CONFIGURATION TO THIS TO RUN: -Djava.library.path=Libs\Natives\Windows --enable-preview --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED
+//IF USING LINUX CHANGE Libs\Natives\Windows WITH Libs\Natives\Linux
+
 public class Main {
 
     private static final SingleOrCoopSelectPage selectSingleOrCoop = new SingleOrCoopSelectPage();
