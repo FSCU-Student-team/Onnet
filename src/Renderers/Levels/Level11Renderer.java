cb0d8b4d2b1e661f7c94a6199f45cceb06d59306
Ahmed Reda <ahmedbinreda@gmail.com>
2025-12-11 19:44:59 +0200

    Update Level11Renderer.java (Adjust POWER_INCREMENT as default = 0.4)

--- src/Renderers/Levels/Level11Renderer.java
+++ src/Renderers/Levels/Level11Renderer.java
@@ -27,7 +27,7 @@ public class Level11Renderer implements GLEventListener, GameLoop {
 
     // Tunables
     private static final double MAX_POWER = 100.0;
-    private static final double POWER_INCREMENT = 0.6;
+    private static final double POWER_INCREMENT = 0.4;
     private static final double ANGLE_INCREMENT = 0.25;
     private static final double POWER_SCALE = 0.03;
 
